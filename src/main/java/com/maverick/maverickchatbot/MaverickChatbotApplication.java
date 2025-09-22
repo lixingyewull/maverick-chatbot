0177f32ff45de3c3439b31f50748d944e230ba95
lixingye <lixingye@gktedu.com>
2025-09-22 22:58:15 +0800

    init

---
+++ src/main/java/com/maverick/maverickchatbot/MaverickChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.maverick.maverickchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverickChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaverickChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maverick/maverickchatbot/MaverickChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverick.maverickchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverickChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
